01be608ad8fc439c0574c4213d31d5814305aaaa
Massimiliano Ianniello <massimilianoianniello@gmail.com>
2025-12-19 12:42:40 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
